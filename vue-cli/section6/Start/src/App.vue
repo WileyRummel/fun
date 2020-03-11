f5ad0bfe422f18872298443a9dcfb537fdc84c52
WileyRummel <wiley.rummel@gmail.com>
2020-03-11 12:35:38 -0700

    dont merge this

---
+++ vue-cli/section6/Start/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <QuoteHeader></QuoteHeader>
+        <p>Hello</p>
+    
+    </div>
+</template>
+
+<script>
+    import QuoteHeader from "./components/QuoteHeader.vue"
+    export default {
+
+        components: {
+            QuoteHeader
+        }
+        
+    }
+</script>
+//add info about clicking to delete on the bottom
+
+<style>
+</style>
+ 
---
+++ vue-cli/section6/Start/src/components/QuoteHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bar">
+        <h3> Quotes Added </h3>
+        <hr>
+        <div class="boxwrap">
+        <p> {{ quoteNum() }} /10 </p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            QuotesAdded: ["hello"],
+
+        }
+    },
+    methods: {
+        quoteNum() {
+            return QuotesAdded.length
+        }
+    },    
+}
+</script>
+
+<style scoped>
+
+    .boxwrap {
+        border: 1px solid darkgray;
+        border-radius: 2%;
+        background-color: lightgray;
+    }
+    p {
+        border: 1px solid darkgray;
+        border-radius: 2%;
+        background-color: lightblue;
+        width: 10%;
+
+    }
+</style>
